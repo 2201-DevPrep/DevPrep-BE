6e24264a68621d861f3fe02269f5d5b8bd2a77ad
Brylan Gannon <brylan.gannon112@gmail.com>
2022-07-14 20:55:23 -0600

    Add db.create_all() to the test file for CircleCI

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,6 +1,8 @@
 from app import app, db, User
 import json
 # from models import UserModel, CardModel
+db.create_all()
+
 def test_register_user():
     users = User.query.all()
     for user in users:
